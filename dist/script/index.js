a69954fe4d1e3880d4a0186892212004d6b9a4b7
yaoshun <yaoshun_d@qq.com>
2016-12-10 17:42:43 +0800

    首页完成

--- dist/script/index.js
+++ dist/script/index.js
@@ -1,5 +1,40 @@
-(function(){
-	console.info("Hello World");
+$(function(){
+    // 特色轮播特效
+	var teseSwiper = $('.tese-carousel').swiper({
+		loop: true,
+		autoplay: 3000,
+		autoplayDisableOnInteraction: false,
+		pagination: '.pagination',
+    	paginationClickable: true
+	});
+    // 新闻轮播特效
+	var newsSwiper = $('.news-carousel').swiper({
+		loop: true,
+		autoplay: 3000,
+		autoplayDisableOnInteraction: false,
+		pagination: '.pagination2',
+    	paginationClickable: true
+	});
+    // 为切换Tab页绑定事件
+	$('.news > .selector > span[class!=more] > a').on('click', function(e){
+	  e.preventDefault();
 
-	document.getElementById("app").innerHTML = "<h4>" + new Date + "</h4>"
-})();
+    $('.news > .selector > span[class!=more]').removeClass('active');
+    $(this).parent().addClass('active');
+    
+    var dataTag = $(this).attr('data-tag');
+    
+    $(".news > .content").hide();
+    $("#" + dataTag).show();
+	});
+	
+	$('a[data-tag=news]').click();
+
+
+    //点击预约
+    $("#order-submit").click(function(){
+        alert('预约手机号：'+$('#order-mobile').val());
+    });
+     
+
+})
