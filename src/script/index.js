50d47835cd90e6ebf4774d0f27a81e61945cf569
yaoshun <yaoshun_d@qq.com>
2016-12-12 13:25:19 +0800

    预约弹窗

--- src/script/index.js
+++ src/script/index.js
@@ -33,8 +33,25 @@ $(function(){
 
     //点击预约
     $("#order-submit").click(function(){
-        alert('预约手机号：'+$('#order-mobile').val());
+        var mobile = $('#order-mobile').val();
+        if(!mobile){
+            $('.error').text('请输入手机号');
+            return;
+        }
+        if(!mobile.match(/^1\d{10}$/)){
+	        $('.error').text('您填写的手机格式不正确');
+            return;
+        }
+
+        //成功之后处理
+        $('#order-mobile').val('');
+        $('.error').text('');
+        alert('预约成功');
     });
+
+    $('#order-mobile').focus(function(){
+        $('.error').text('');
+    })
      
     //动画
     $('.first-page .person').addClass('fadeRight');
